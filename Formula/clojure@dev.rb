6d3e6ec39b07d04edfe46fc7d67b077a2d0e51ae
Alex Miller <alex.miller@cognitect.com>
2020-02-26 10:24:20 -0600

    dev tracking

--- Formula/clojure@dev.rb
+++ Formula/clojure@dev.rb
@@ -1,7 +1,7 @@
 class Clojure < Formula
   desc "The Clojure Programming Language"
   homepage "https://clojure.org"
-  url "https://download.clojure.org/install/clojure-tools-1.10.1.507.tar.gz"
+  url "https://download.clojure.org/install/clojure-tools-1.10.1.510.tar.gz"
   sha256 "c6e003f612bdd7f9a9baa6d86deafb2d51b411310077c83c9ed13bc649c13b18"
 
   bottle :unneeded
