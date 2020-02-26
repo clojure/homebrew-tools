5ee0ef2f94fc616958f382c4d499014ce7acb364
Alex Miller <alex.miller@cognitect.com>
2020-02-26 10:27:30 -0600

    make dev formula unique

--- Formula/clojure@dev.rb
+++ Formula/clojure@dev.rb
@@ -1,4 +1,4 @@
-class Clojure < Formula
+class ClojureATdev < Formula
   desc "The Clojure Programming Language"
   homepage "https://clojure.org"
   url "https://download.clojure.org/install/clojure-tools-1.10.1.510.tar.gz"
