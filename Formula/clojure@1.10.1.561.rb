78c065294577eb8cabcfba1154deb68e2e018848
Hudson @ build.clojure.org <build@clojure.com>
2020-07-17 14:44:14 -0500

    Publish 1.10.1.561

--- Formula/clojure@1.10.1.561.rb
+++ Formula/clojure@1.10.1.561.rb
@@ -1,14 +1,8 @@
-class Clojure < Formula
+class ClojureAT1101561 < Formula
   desc "The Clojure Programming Language"
   homepage "https://clojure.org"
-  url "https://download.clojure.org/install/clojure-tools-1.10.1.547.tar.gz"
-  sha256 "2012b44ebdb9c7e49bfc03fec9571b88dbc1c1c2e589d4eba2fd3b80c7278f1a"
-
-  devel do
-    url "https://download.clojure.org/install/clojure-tools-1.10.1.561.tar.gz"
-    sha256 "46c193d09f8fce9c0e5e530c2463586cec9717593cdee2f57199d278ed104e2d"
-    version "1.10.1.561"
-  end
+  url "https://download.clojure.org/install/clojure-tools-1.10.1.561.tar.gz"
+  sha256 "46c193d09f8fce9c0e5e530c2463586cec9717593cdee2f57199d278ed104e2d"
 
   bottle :unneeded
 
