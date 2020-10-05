71e83b5283e75384b617e548d9c49b6ccd515ffa
Hudson @ build.clojure.org <build@clojure.com>
2020-10-05 17:31:53 -0500

    Publish 1.10.1.708

--- Formula/clojure@1.10.1.708.rb
+++ Formula/clojure@1.10.1.708.rb
@@ -1,8 +1,8 @@
-class Clojure < Formula
+class ClojureAT1101708 < Formula
   desc "The Clojure Programming Language"
   homepage "https://clojure.org"
-  url "https://download.clojure.org/install/clojure-tools-1.10.1.697.tar.gz"
-  sha256 "700dd4a4fad95eda3392fd9f6026bbb4b0a3f31d7bf6c01324f225623f365974"
+  url "https://download.clojure.org/install/clojure-tools-1.10.1.708.tar.gz"
+  sha256 "e200f3e148918a12226026809f052f1be54bd43d490bb079072182488155dbc9"
   license "EPL-1.0"
 
   bottle :unneeded
