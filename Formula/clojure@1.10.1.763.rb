da5786708f3160491f5194e67b934dacd8aa337d
Hudson @ build.clojure.org <build@clojure.com>
2020-12-10 13:44:44 -0600

    Publish 1.10.1.763

---
+++ Formula/clojure@1.10.1.763.rb
@@ -0,0 +1,25 @@
+class ClojureAT1101763 < Formula
+  desc "The Clojure Programming Language"
+  homepage "https://clojure.org"
+  url "https://download.clojure.org/install/clojure-tools-1.10.1.763.tar.gz"
+  sha256 "2a3ec8a6c3639035c2bba10945ae9007ab0dc9136766b95d2161f354e62a4d10"
+  license "EPL-1.0"
+
+  bottle :unneeded
+
+  depends_on "rlwrap"
+
+  uses_from_macos "ruby" => :build
+
+  def install
+    system "./install.sh", prefix
+  end
+
+  test do
+    ENV["TERM"] = "xterm"
+    system("#{bin}/clj -M -e nil")
+    %w[clojure clj].each do |clj|
+      assert_equal "2", shell_output("#{bin}/#{clj} -M -e \"(+ 1 1)\"").strip
+    end
+  end
+end
