07968083aa8cc7463b7298d08762b58640930c09
Hudson @ build.clojure.org <build@clojure.com>
2020-12-17 13:07:29 -0600

    Promote 1.10.1.763 to stable

--- Formula/clojure.rb
+++ Formula/clojure.rb
@@ -1,8 +1,8 @@
 class Clojure < Formula
   desc "The Clojure Programming Language"
   homepage "https://clojure.org"
-  url "https://download.clojure.org/install/clojure-tools-1.10.1.754.tar.gz"
-  sha256 "55105788a264e2760d1c0ba74e166f425d98e3964e2696a33b97f230133b72c0"
+  url "https://download.clojure.org/install/clojure-tools-1.10.1.763.tar.gz"
+  sha256 "2a3ec8a6c3639035c2bba10945ae9007ab0dc9136766b95d2161f354e62a4d10"
   license "EPL-1.0"
 
   bottle :unneeded
