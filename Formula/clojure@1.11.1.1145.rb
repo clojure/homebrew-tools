2f4e2bc0934446fbf0ef625855f68505bde6dff8
Clojure Build <clojure-build@clojure.com>
2022-06-20 02:25:15 -0500

    Publish 1.11.1.1145

---
+++ Formula/clojure@1.11.1.1145.rb
@@ -0,0 +1,23 @@
+class ClojureAT11111145 < Formula
+  desc "The Clojure Programming Language"
+  homepage "https://clojure.org"
+  url "https://download.clojure.org/install/clojure-tools-1.11.1.1145.tar.gz"
+  sha256 "f94209a3ee0d516dc42335cb13c925df425e463149f69257213bd2153cf81524"
+  license "EPL-1.0"
+
+  depends_on "rlwrap"
+
+  uses_from_macos "ruby" => :build
+
+  def install
+    system "./install.sh", prefix
+  end
+
+  test do
+    ENV["TERM"] = "xterm"
+    system("#{bin}/clj -M -e nil")
+    %w[clojure clj].each do |clj|
+      assert_equal "2", shell_output("#{bin}/#{clj} -M -e \"(+ 1 1)\"").strip
+    end
+  end
+end
