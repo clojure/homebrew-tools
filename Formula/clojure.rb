2df455ec55381342859b97ab67aa218fb91b65ec
Clojure Build <clojure-build@clojure.com>
2023-03-15 17:18:17 -0500

    Promote 1.11.1.1257 to stable

--- Formula/clojure.rb
+++ Formula/clojure.rb
@@ -1,8 +1,8 @@
 class Clojure < Formula
   desc "The Clojure Programming Language"
   homepage "https://clojure.org"
-  url "https://download.clojure.org/install/clojure-tools-1.11.1.1252.tar.gz"
-  sha256 "65016137fbced4bd642a6102eb0293ef8aa2991e9cb647685eb085ba3a1b5fa0"
+  url "https://download.clojure.org/install/clojure-tools-1.11.1.1257.tar.gz"
+  sha256 "6d970916d0cea3c4b62d079bb1d4e483394156e65a2895255105f817fa92abd0"
   license "EPL-1.0"
 
   depends_on "rlwrap"
