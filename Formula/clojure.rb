a1c0df0dd0429f2eb026264f50ab0c825a4eae08
Alex Miller <alex.miller@cognitect.com>
2023-08-14 00:37:02 -0500

    switch formulas back to using download.clojure.org for now

--- Formula/clojure.rb
+++ Formula/clojure.rb
@@ -1,8 +1,8 @@
 class Clojure < Formula
   desc "The Clojure Programming Language"
   homepage "https://clojure.org"
-  url "https://github.com/clojure/brew-install/releases/download/1.11.1.1369/clojure-tools-1.11.1.1369.tar.gz"
-  mirror "https://download.clojure.org/install/clojure-tools-1.11.1.1369.tar.gz"
+  mirror "https://github.com/clojure/brew-install/releases/download/1.11.1.1369/clojure-tools-1.11.1.1369.tar.gz"
+  url "https://download.clojure.org/install/clojure-tools-1.11.1.1369.tar.gz"
   sha256 "e4187f7c4091fcb95c6b42a23e852dc95736c7b91a4835388c80995cc138676e"
   license "EPL-1.0"
 
