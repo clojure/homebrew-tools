d9bc4e1e16285c8134d55d929afc7f79600437f4
clojure-build <clojure-build@users.noreply.github.com>
2024-03-08 23:29:53 +0000

    Promote 1.11.2.1446 to stable

--- Formula/clojure.rb
+++ Formula/clojure.rb
@@ -1,9 +1,9 @@
 class Clojure < Formula
   desc "The Clojure Programming Language"
   homepage "https://clojure.org"
-  url "https://github.com/clojure/brew-install/releases/download/1.11.2.1441/clojure-tools-1.11.2.1441.tar.gz"
-  mirror "https://download.clojure.org/install/clojure-tools-1.11.2.1441.tar.gz"
-  sha256 "ba88acc4a2be27cd108ad0bafd47e68f18f41d0ab63f557aeb1166a6ca9b75b7"
+  url "https://github.com/clojure/brew-install/releases/download/1.11.2.1446/clojure-tools-1.11.2.1446.tar.gz"
+  mirror "https://download.clojure.org/install/clojure-tools-1.11.2.1446.tar.gz"
+  sha256 "aa7effb0fc950df8d93cb796c655010658c05bf77c702c3a9449b7fdd792ef71"
   license "EPL-1.0"
 
   depends_on "rlwrap"
