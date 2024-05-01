b4c43ea89d8e9938976291f1b2b03aa8e4a7d298
clojure-build <clojure-build@users.noreply.github.com>
2024-05-01 18:11:03 +0000

    Publish 1.11.3.1463

---
+++ Formula/clojure@1.11.3.1463.rb
@@ -0,0 +1,24 @@
+class ClojureAT11131463 < Formula
+  desc "The Clojure Programming Language"
+  homepage "https://clojure.org"
+  url "https://github.com/clojure/brew-install/releases/download/1.11.3.1463/clojure-tools-1.11.3.1463.tar.gz"
+  mirror "https://download.clojure.org/install/clojure-tools-1.11.3.1463.tar.gz"
+  sha256 "dba419de3e785f3b695b6589d7183419cf8ec2bb2f99f2b88afd06b0b57c1487"
+  license "EPL-1.0"
+
+  depends_on "rlwrap"
+
+  uses_from_macos "ruby" => :build
+
+  def install
+    system "./install.sh", prefix
+  end
+
+  test do
+    ENV["TERM"] = "xterm"
+    system("#{bin}/clj -M -e nil")
+    %w[clojure clj].each do |clj|
+      assert_equal "2", shell_output("#{bin}/#{clj} -M -e \"(+ 1 1)\"").strip
+    end
+  end
+end
